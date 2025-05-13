af6bd8b133775d66082d1be1190a95e702365beb
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-05-13 12:42:12 +0800

    add ch02.py ch04.py

---
+++ experiment_notebook/ch02.py
@@ -0,0 +1,57 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import tiktoken
+
+
+class GPT2DatasetV1(Dataset):
+  def __init__(self,
+               text,
+               tokenizer,
+               context_length, # length of each input vector
+               stride # chunk the text into overlapping sequence of context_length
+               ):
+    self.input_id_vectors = []
+    self.target_id_vectors = []
+
+    # tokenize the entire text
+    token_list = tokenizer.encode(text)
+
+    # append input and target vectors
+    for i in range(0, len(token_list) - context_length, stride):
+      input_vector = token_list[i:i+context_length]
+      target_vector = token_list[i+1:i+context_length+1]
+      self.input_id_vectors.append(torch.tensor(input_vector))
+      self.target_id_vectors.append(torch.tensor(target_vector))
+
+  # get the number of input vectors
+  def __len__(self):
+    return len(self.input_id_vectors)
+
+  # return the (input vector, target vector) pair
+  def __getitem__(self, id):
+    return self.input_id_vectors[id], self.target_id_vectors[id]
+  
+
+def create_dataloader_V1(text,
+                 batch_size=4,
+                 context_length=256,
+                 stride=128,
+                 shuffle=True, # shuffle dataset
+                 drop_last=True, # drop last batch if it not equal required size
+                 num_workers=0 # number of CPU processes for preprocessing
+                 ):
+
+  tokenizer = tiktoken.get_encoding("gpt2")
+
+  dataset = GPT2DatasetV1(text=text,
+                          tokenizer=tokenizer,
+                          context_length=context_length,
+                          stride=stride)
+
+  dataloader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=shuffle,
+                          drop_last=drop_last,
+                          num_workers=num_workers)
+
+  return dataloader
